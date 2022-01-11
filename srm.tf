2586564cc63c0c6be8ef661ffcf070800c3fb895
jpsrm <97506106+jpsrm@users.noreply.github.com>
2022-01-11 15:03:54 +0530

    Update srm.tf

--- srm.tf
+++ srm.tf
@@ -1,27 +1,35 @@
 provider "aws" {
   region = "ap-southeast-1"
   }
+
 variable "acc_no" {
   type = string
 }
+
 variable "access_key" {
   type = string
 }
+
 variable "secret_access_key" {
   type = string
 }
+
 resource "aws_iam_user" "sowmi"{
     name = "sowmi"
 	}
+
 resource "aws_iam_user" "subha"{
     name = "subha"
 	}
+
 resource "aws_iam_user" "saran"{
     name = "saran"
 	}
+
 resource "aws_iam_group" "cse"{
     name = "cse"
 	}
+
 resource "aws_iam_group_membership" "team" {
   name = "tf-testing-group-membership"
 
